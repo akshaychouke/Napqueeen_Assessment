6113eb9af3abb5c2f4da3bfd9d3f926803f413db
Akshay Chouke <akshaychauke50@gmail.com>
2024-04-10 22:09:20 +0530

    add the vercel config file

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 const connectDB = require("./config/db.js");
 const blogRouter = require("./routes/postRoutes.js");
 dotenv.config();
-
 const app = express();
 connectDB();
 
 app.use(express.json());
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 app.use("/api/posts", blogRouter);
 
 app.listen(process.env.PORT, () => {
